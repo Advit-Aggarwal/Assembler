0f08f8d664d2bcd78e9847270e4afb0ea2cc8de8
Advit Aggarwal <advitaggarwal6@gmail.com>
2025-02-13 18:47:04 +0530

    Update Assembler.py

--- Assembler.py
+++ Assembler.py
@@ -1,7 +1,21 @@
 with open("testcases.txt", "r") as file:
     lines = file.readlines()
 def twos_compliment(value):
-    pass
+    str1 = ""
+    str2 = str(value)
+    n = len(str2)
+    for i in range(n - 1, 0, -1):
+        if value[i] == "0":
+            str1 = "0" + str1 
+        elif value[i] == "1":
+            str1 = "1" + str1
+            break
+    for j in range(i, 0, -1):
+        if value[j] == "0":
+            str1 = "1" + str1
+        elif value[j] == "1":
+            str1 = "0" + str1
+    return str1
 def num_to_binary(value, num):
     binary = []
     temp = value
